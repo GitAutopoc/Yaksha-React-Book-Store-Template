d1571554c08674f65e4ff1804b884b7dfab7f0ba
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-07-16 20:38:49 +0530

    Add files via upload

---
+++ src/components/Admin Panel/AdminOrderList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AdminOrderList = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default AdminOrderList;
---
+++ src/components/Book/BookForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BookForm = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default BookForm;
